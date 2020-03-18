22b8906fe9672b7434f626792da63d8e2cb5e47e
Shota Ise <nightowl0207@gmail.com>
2020-03-18 21:40:01 +0900

    learned about React props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
 
-// class App extends Component {
-//   render() {
-//     return <div>Hello, world!</div>
-//   };
-// }
-
 const App = () => {
+  const profiles = [
+    { name: "太郎", age: 10 },
+    { name: "さやか", age: 25 },
+    { name: "花子", age: 21 },
+    {}
+  ]
   return (
-    <React.Fragment>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
-    </React.Fragment>
+    <div>
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index}/>
+        })
+      }
+    </div>
   )
 }
 
-const Cat = () => {
-  return <div>Meow!</div>;
+const User = (props) => {
+  return <div>Hi, I am {props.name}, and I am {props.age} years old!</div>;
+}
+
+User.defaultProps = {
+  name: "John Doe",
+  age: 1
 }
 
 export default App;
