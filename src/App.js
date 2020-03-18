8576c02ca2b5705580ece9b87c9bd504532d8feb
Shota Ise <nightowl0207@gmail.com>
2020-03-18 21:50:06 +0900

    learned how to use props and define proptypes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const App = () => {
   const profiles = [
     { name: "太郎", age: 10 },
     { name: "さやか", age: 25 },
     { name: "花子", age: 21 },
-    {}
   ]
   return (
     <div>
@@ -22,9 +22,9 @@ const User = (props) => {
   return <div>Hi, I am {props.name}, and I am {props.age} years old!</div>;
 }
 
-User.defaultProps = {
-  name: "John Doe",
-  age: 1
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number
 }
 
 export default App;
