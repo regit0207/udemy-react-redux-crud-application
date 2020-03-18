afded172f43ff540811b82838e0767a33f1d008a
Shota Ise <nightowl0207@gmail.com>
2020-03-18 21:18:30 +0900

    added functional component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return <div>Hello, world!</div>
-  };
+// class App extends Component {
+//   render() {
+//     return <div>Hello, world!</div>
+//   };
+// }
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </React.Fragment>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>;
 }
 
 export default App;
